ad199107fc4cd63b5d2a2d03768a671aca7bfaa3
Sarim Khalil <shktv03@gmail.com>
2022-01-04 21:20:52 +0000

    added top elo

--- warmod-elo-bot/main.py
+++ warmod-elo-bot/main.py
@@ -1,6 +1,7 @@
 import parselogfiles
 import compiledata
 import queries
+import topelo
 
 import discord
 from discord.ext import commands
@@ -21,20 +22,29 @@ queries.create_table(c, conn)
 
 def open_key():
     with open("keys.txt", "r") as f:
-        key = f.readline()
-        key = key.split(":")[0]
-    return key
+        keys = f.readlines()
+        discordkey = keys[0].split(":")[0]
+        steamkey = keys[1].split(":")[0]
+    return discordkey, steamkey
+
+discordkey, steamkey = open_key()
 
 @bot.event
 async def on_ready():
     print(f"logged in as {bot.user}")
 
-@tasks.loop(seconds=10)
+@tasks.loop(seconds=1)
 async def data_parser():
     gamestats = parselogfiles.run()
     if gamestats:
         compiledata.run(gamestats, c, conn)
 
+@bot.command()
+async def top_elo(ctx):
+    pages = topelo.run(steamkey, c)
+    for page in pages:
+        await ctx.message.channel.send(page)
+
 if __name__ == "__main__":
     data_parser.start()
-    bot.run(open_key())
+    bot.run(discordkey)
