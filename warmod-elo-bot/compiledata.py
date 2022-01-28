1da625ce7467efdba0ad9887765bf70dc585a591
Sarim Khalil <shk17@student.le.ac.uk>
2022-01-28 15:48:49 +0000

    consulted wikipedia elo page

--- warmod-elo-bot/compiledata.py
+++ warmod-elo-bot/compiledata.py
@@ -6,14 +6,13 @@ def run(gamestats, c, conn):
     add_players_to_db_if_not_exist(gamestats, c, conn)
     teamelo = get_avg_team_elo(gamestats, c)
     win_percentage = get_win_chances(teamelo, winner_team_id, loser_team_id)
-    net_elo_gain = int(50 * win_percentage)
-    net_elo_loss = 50 - net_elo_gain
+    net_elo_change = int(50 * (1-win_percentage))
 
     for player_id in gamestats["playerstats"]:
         if gamestats["playerstats"][player_id]["team_id"] == winner_team_id:
-            queries.update_elo_wins_losses(net_elo_gain, True, player_id, c)
+            queries.update_elo_wins_losses(net_elo_change, True, player_id, c)
         else:
-            queries.update_elo_wins_losses(net_elo_loss, False, player_id, c)
+            queries.update_elo_wins_losses(net_elo_change, False, player_id, c)
     conn.commit()
 
     # updating kda and clutches
@@ -45,15 +44,21 @@ def get_avg_team_elo(gamestats, c):
     for player_id in gamestats["playerstats"]:
         if not gamestats["playerstats"][player_id]["team_id"] in teamelo:
             teamelo[gamestats["playerstats"][player_id]["team_id"]] = []
-        else:
-            player_elo = queries.elo_from_user_id(player_id, c)[0]
-            teamelo[gamestats["playerstats"][player_id]["team_id"]].append(player_elo)
 
+        player_elo = queries.elo_from_user_id(player_id, c)
+        if player_elo is None:
+            player_elo = 1000
+        else:
+            player_elo = player_elo[0]
+
+        teamelo[gamestats["playerstats"][player_id]["team_id"]].append(player_elo)
+
+    print(teamelo)
     for team_id in teamelo:
         teamelo[team_id] = sum(teamelo[team_id]) / len(teamelo[team_id])
     return teamelo
 
 def get_win_chances(teamelo, winner_team_id, loser_team_id):
-    win_percentage = round(((teamelo[winner_team_id] / teamelo[loser_team_id]) / 2) , 2)
+    win_percentage = 1 / (1 + 10 ** ((teamelo[loser_team_id] - teamelo[winner_team_id]) / 400))
     win_percentage = max(min(win_percentage, 0.99), 0.01)
     return win_percentage
